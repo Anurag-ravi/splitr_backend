02326f4d2a256f3b7d0179d099e2e4827466f006
Anurag Ravi <kumarayush1014@gmail.com>
2024-01-19 21:27:26 +0530

    added deploy script

---
+++ .scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Pull the latest version of the app
+git pull origin master
+echo "New changes copied to server !"
+
+# change directory to the app
+cd /home/anurag/splitr_backend
+
+# Install dependencies
+echo "Installing Dependencies..."
+npm install --yes
+
+# restart pm2
+echo "Restarting pm2..."
+pm2 restart all
+
+echo "Deployment Finished!"
