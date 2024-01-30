4ad28c2ce1da0a9548f65df818f9aa42de9bca4f
Anurag Ravi <kumarayush1014@gmail.com>
2024-01-30 18:02:01 +0530

    logic for creating expense

--- src/controllers/expense.js
+++ src/controllers/expense.js
@@ -1,9 +1,46 @@
-const createExpense = async (req,res) => {
-    const {trip,name,amount,category,split_type,paid_by,paid_for} = req.body;
-    console.log(trip,name,amount,category,split_type,paid_by,paid_for);
-    res.json({status:200});
-}
+const Expense = require("../models/expense");
+const Trip = require("../models/trip");
+
+const createExpense = async (req, res) => {
+  const { trip, name, amount, category, split_type, paid_by, paid_for } =
+    req.body;
+  if (
+    !trip ||
+    !name ||
+    !amount ||
+    !category ||
+    !split_type ||
+    !paid_by ||
+    !paid_for
+  )
+    return res.json({ status: 400, message: "Fill all the fields" });
+  const tripObj = await Trip.findOne({ _id, trip }).populate({
+    path: "users",
+    model: "TripUser",
+  });
+  if (!tripObj) return res.json({ status: 400, message: "Trip not found" });
+
+  var user_involved = false;
+  tripObj.users.map((tripUser) => {
+    if (tripUser._id === req.user._id) {
+      user_involved = true;
+    }
+  });
+  if (!user_involved)
+    return res.json({ status: 400, message: "Not involved in trip" });
+
+  const expense = await Expense.create({
+    trip,
+    name,
+    amount,
+    category,
+    split_type,
+    paid_by,
+    paid_for,
+  });
+  return res.json({ status: 200, data: expense });
+};
 
 module.exports = {
-    createExpense
-}
+  createExpense,
+};
